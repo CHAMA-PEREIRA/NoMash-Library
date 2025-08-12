76232fabd799bbf09ecb24fbd8c3a3a0f8cd6b92
CHAMA-PEREIRA <hmen0014@student.monash.edu>
2025-08-12 18:37:47 +0800

    Fix base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/nomash-library/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/NoMash-Library/' : '/',
   plugins: [
     vue(),
     vueDevTools(),
